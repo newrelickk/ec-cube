67f33c24b245a97619649c785226356e5c9f3e1a
Takuya Azuma <tazuma@newrelic.com>
2023-05-02 10:57:45 +0900

    add run script

---
+++ scripts/run.js
@@ -0,0 +1,16 @@
+#!/bin/bash
+. ~/.bashrc
+cd ~/ec-cube/ec-cube
+php bin/console server:run --env=dev -q -- 0.0.0.0:8000 &
+echo "サンプルサービスが起動しました。 http://$HOSTNAME.$_SANDBOX_ID.instruqt.io:8000 にアクセスしてみましょう。"
+export PUPPETEER_EXECUTABLE_PATH=/usr/bin/chromium-browser
+nvm install 16 && nvm use 16
+cd ~/ec-cube/scripts/puppeteer
+echo "自動テスト環境準備中"
+npm i --silent
+echo "自動テスト環境準備完了"
+echo "テストユーザーを作成します。"
+node createUsers.js
+echo "テストユーザーを作成しました。"
+echo "自動テストを開始します。"
+node test.js &
