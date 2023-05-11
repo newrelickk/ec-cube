99ddef77f6981668ea5a204bb7b9346b057d059c
Takuya Azuma <tazuma@newrelic.com>
2023-05-11 13:05:50 +0900

    report error

--- src/Eccube/EventListener/ExceptionListener.php
+++ src/Eccube/EventListener/ExceptionListener.php
@@ -48,8 +48,9 @@ class ExceptionListener implements EventSubscriberInterface
         $statusCode = Response::HTTP_INTERNAL_SERVER_ERROR;
 
         $exception = $event->getException();
-　　　　　if (extension_loaded('newrelic')) {
-          newrelic_notice_error ($exception);
+
+        if (extension_loaded('newrelic')) {
+            newrelic_notice_error($exception);
         }
         if ($exception instanceof HttpExceptionInterface) {
             $statusCode = $exception->getStatusCode();
