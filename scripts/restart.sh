c3978d12e77e2b293186ec41199a3b72a9f81086
Takuya Azuma <tazuma@newrelic.com>
2023-06-07 09:18:21 +0900

    restart server

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -7,5 +7,6 @@ if [ -f /tmp/service.pid ]; then
   sleep 5;
 fi
 echo "サンプルサービスを起動します。しばらくお待ちください。"
-php bin/console server:run --env=dev -q -- 0.0.0.0:8000 &
+nohup php bin/console server:run --env=dev -q -- 0.0.0.0:8000 &
 echo $! > /tmp/service.pid
+echo "サンプルサービスが起動しました。 http://$HOSTNAME.$_SANDBOX_ID.instruqt.io:8000 にアクセスしてみましょう。"
